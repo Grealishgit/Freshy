6b999bb285fc96d6f0f30294a831c4c890943f3f
Hunter <eugyneehunter@gmail.com>
2025-07-19 02:14:30 +0300

    Add BannerCards component and associated styles to enhance the Home page layout

---
+++ src/component/BannerCards.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './BannerCards.css'
+
+const BannerCards = () => {
+    return (
+        <div className='banner-cards'>
+            <div className='banner'>
+                <p>
+                    We prioritize freshness and health by using locally sourced ingredients.
+                    Our dishes are crafted with care, ensuring that natural flavors shine. Every meal is prepared
+                    with a commitment to quality and well-being. Enjoy a pure and wholesome dining experience with us!
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default BannerCards
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../component/Header'
+import BannerCards from '../component/BannerCards'
 
 const Home = () => {
     return (
         <>
             <Header />
+            <BannerCards />
         </>
     )
 }
